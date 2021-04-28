a94c17b6cd7dcec2f4f12bab43d3d6876ed47bbd
SamanAtashi <milady00126@gmail.com>
2021-04-28 18:45:37 +0300

    Fix linters _ 1

--- bubble_sorts.rb
+++ bubble_sorts.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 def bubble_sort(arr)
   sorted = false
   until sorted
@@ -13,20 +15,20 @@ def bubble_sort(arr)
 end
 
 def bubble_sort_by(arr)
-if arr.empty? or arr.nil?
-    p "no block given"
-else
-  sorted = false
-  until sorted
-    sorted = true
-    (arr.length - 1).times do |index|
-      next unless arr[index].length > arr[index + 1].length
+  if arr.empty? || arr.nil?
+    p 'no block given'
+  else
+    sorted = false
+    until sorted
+      sorted = true
+      (arr.length - 1).times do |index|
+        next unless arr[index].length > arr[index + 1].length
 
-      arr[index], arr[index + 1] = arr[index + 1], arr[index]
-      sorted = false
+        arr[index], arr[index + 1] = arr[index + 1], arr[index]
+        sorted = false
+      end
     end
+    p arr
   end
-  p arr
-end  
 end
 bubble_sort_by([])
