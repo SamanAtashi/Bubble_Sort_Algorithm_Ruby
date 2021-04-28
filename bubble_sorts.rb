0b6c4f2a8010c0d12048cfc1032729718281b6d5
danmainah <danielmainawaweru@gmail.com>
2021-04-28 11:20:37 +0300

    Setting up the method

---
+++ bubble_sorts.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+def bubble_sort (arr)
+# creating the method
+#for loop to iterate through the arr
+ for item in arr
+    #check if arr is sorted dont touch if not start a loop
+    print item
+
+
+ end
+
+end
+#compare two items like a and b
+
+# if a is more than b ,we swap their position if not we dont touch it
+
+# return the sorted array
