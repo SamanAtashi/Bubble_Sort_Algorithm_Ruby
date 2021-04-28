4e8bc470436380d055e12df0c2713bd174c5ba24
danmainah <danielmainawaweru@gmail.com>
2021-04-28 14:00:06 +0300

    Remove errors in sort function

--- bubble_sorts.rb
+++ bubble_sorts.rb
@@ -1,40 +1,25 @@
 # frozen_string_literal: true
 
-def swap(a,b)
-    a,b=b,a
-    return a,b
-end
-
-# creating the method
 def bubble_sort(arr)
-
-swapped=[]
-
-#! loop to iterate through the arr
-		(arr.length - 1).times do |items|
-
-#! check if arr is sorted dont touch if not start a loop
-			unless arr[items] < arr[items + 1]
-
-
-p "this is before condition #{arr}"
-
-
-#! compare two items like a and b
-                if arr[items] > arr[items+1]
-
-#! if a is more than b ,we swap their position if not we dont touch it
-                    git swapped = swap(arr[items],arr[items+1])
-                end
-            end
+    #check if array is sorted using sort method
+    sorted = false
+    #use while to loop the arrays
+	while !sorted
+		sorted = true
+        #sort the array until sort is true
+		(arr.length - 1).times do |index|
+			if arr[index] > arr[index + 1]
+				arr[index], arr[index+1] = arr[index+1], arr[index]
+                #allow swapping until the sort array is achieved
+				sorted = false
+			end
 		end
-p swapped
-
-
-
+	end
+    #display the sorted arr
+	p arr
 end
 # todo: do not forget to return array if it was sorted
 
 # bubble_sort([1, 2, 3, 4])
-bubble_sort([4, 3 , 5 , 6])
-# bubble_sort([4, 3, 78, 2, 0, 2])
+# bubble_sort([4, 3 , 5 , 6])
+bubble_sort([4, 3, 78, 2, 0, 2])
