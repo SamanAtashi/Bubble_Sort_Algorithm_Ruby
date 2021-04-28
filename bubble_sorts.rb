5d41b603d19f53db65f379823486b3057da20fde
SamanAtashi <milady00126@gmail.com>
2021-04-28 12:11:39 +0300

    Logics till swapping

--- bubble_sorts.rb
+++ bubble_sorts.rb
@@ -1,17 +1,27 @@
 # frozen_string_literal: true
-def bubble_sort (arr)
+
 # creating the method
-#for loop to iterate through the arr
- for item in arr
-    #check if arr is sorted dont touch if not start a loop
-    print item
+def bubble_sort(arr)
+#* loop to iterate through the arr
+		(arr.length - 1).times do |items|
+#* check if arr is sorted dont touch if not start a loop
+			unless arr[items] <= arr[items + 1]
+#* compare two items like a and b
+                if arr[items]<arr[items+1]
+#* if a is more than b ,we swap their position if not we dont touch it
+                    # swap
+                    arr[items],arr[items+1]=arr[items+1],arr[items]
+                end
+            end
+		end
+p arr
 
-
- end
 
 end
-#compare two items like a and b
 
-# if a is more than b ,we swap their position if not we dont touch it
 
-# return the sorted array
+# todo: do not forget to return array if it was sorted
+
+# bubble_sort([1, 2, 3, 4])
+bubble_sort([4, 3, 2, 1])
+# bubble_sort([4, 3, 78, 2, 0, 2])
