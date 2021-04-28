bb44402944f690516a1c97c315530bb3a905e05a
SamanAtashi <milady00126@gmail.com>
2021-04-28 15:28:34 +0300

    Add Second task (for block)

--- bubble_sorts.rb
+++ bubble_sorts.rb
@@ -1,24 +1,39 @@
 # frozen_string_literal: true
 
 def bubble_sort(arr)
-    #create an array sorted with boolean condition false
-    sorted = false
-    #use while to loop the arrays
-	while !sorted
-		sorted = true
-        #loop the array until sort is true
-		(arr.length - 1).times do |index|
-			if arr[index] > arr[index + 1]
-				arr[index], arr[index+1] = arr[index+1], arr[index]
-                #allow swapping until the sort array is achieved
-				sorted = false
-			end
-		end
-	end
-    #display the sorted arr
-	p arr
+  # create an array sorted with boolean condition false
+  sorted = false
+  # use while to loop the arrays
+  until sorted
+    sorted = true
+    # loop the array until sort is true
+    (arr.length - 1).times do |index|
+      next unless arr[index] > arr[index + 1]
+
+      arr[index], arr[index + 1] = arr[index + 1], arr[index]
+      # allow swapping until the sort array is achieved
+      sorted = false
+    end
+  end
+  # display the sorted arr
+  p arr
 end
 
-# bubble_sort([1, 2, 3, 4])
-# bubble_sort([4, 3 , 5 , 6])
-bubble_sort([4, 3, 78, 2, 0, 2])
+def bubble_sort_by(arr)
+  # create an array sorted with boolean condition false
+  sorted = false
+  # use while to loop the arrays
+  until sorted
+    sorted = true
+    # loop the array until sort is true
+    (arr.length - 1).times do |index|
+      next unless arr[index].length > arr[index + 1].length
+
+      arr[index], arr[index + 1] = arr[index + 1], arr[index]
+      # allow swapping until the sort array is achieved
+      sorted = false
+    end
+  end
+  # display the sorted arr
+  p arr
+end
