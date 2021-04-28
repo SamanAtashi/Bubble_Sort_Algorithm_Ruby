d33770e2a4d22e575ed72aed399870d7433a17cb
danmainah <danielmainawaweru@gmail.com>
2021-04-28 14:07:57 +0300

    Correct errors in comment section

--- bubble_sorts.rb
+++ bubble_sorts.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 def bubble_sort(arr)
-    #check if array is sorted using sort method
+    #create an array sorted with boolean condition false
     sorted = false
     #use while to loop the arrays
 	while !sorted
 		sorted = true
-        #sort the array until sort is true
+        #loop the array until sort is true
 		(arr.length - 1).times do |index|
 			if arr[index] > arr[index + 1]
 				arr[index], arr[index+1] = arr[index+1], arr[index]
@@ -18,7 +18,6 @@ def bubble_sort(arr)
     #display the sorted arr
 	p arr
 end
-# todo: do not forget to return array if it was sorted
 
 # bubble_sort([1, 2, 3, 4])
 # bubble_sort([4, 3 , 5 , 6])
