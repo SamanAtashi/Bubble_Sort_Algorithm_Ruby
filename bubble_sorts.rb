a352b757febcbcaffafea7ab1a40257f93c78c19
SamanAtashi <milady00126@gmail.com>
2021-04-28 18:32:37 +0300

    Delete comments

--- bubble_sorts.rb
+++ bubble_sorts.rb
@@ -1,37 +1,27 @@
 def bubble_sort(arr)
-  # create an array sorted with boolean condition false
   sorted = false
-  # use while to loop the arrays
   until sorted
     sorted = true
-    # loop the array until sort is true
     (arr.length - 1).times do |index|
       next unless arr[index] > arr[index + 1]
 
       arr[index], arr[index + 1] = arr[index + 1], arr[index]
-      # allow swapping until the sort array is achieved
       sorted = false
     end
   end
-  # display the sorted arr
   p arr
 end
 
 def bubble_sort_by(arr)
-  # create an array sorted with boolean condition false
   sorted = false
-  # use while to loop the arrays
   until sorted
     sorted = true
-    # loop the array until sort is true
     (arr.length - 1).times do |index|
       next unless arr[index].length > arr[index + 1].length
 
       arr[index], arr[index + 1] = arr[index + 1], arr[index]
-      # allow swapping until the sort array is achieved
       sorted = false
     end
   end
-  # display the sorted arr
   p arr
 end
